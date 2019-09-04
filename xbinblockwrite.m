15c58f2c5ff8822ca9a2fefff61b753f58720509
Tianwai@PC <tianwai@kaist.ac.kr>
2019-09-04 16:15:14 +0900

    put the obsolete codes in a subfolder

--- xbinblockwrite.m
+++
@@ -1,8 +0,0 @@
-function xbinblockwrite(f, data, format, cmd)
-% set debugScpi=1 in MATLAB workspace to log SCPI commands
-if (evalin('base', 'exist(''debugScpi'', ''var'')'))
-    fprintf('cmd = %s %s, %d elements\n', cmd, format, length(data));
-end
-binblockwrite(f, data, format, cmd);
-fprintf(f, '');
-end
